bbd81a0535c59a13283e194dd1625d609b23079d
bartoszb0 <bartoszb2020@gmail.com>
2026-05-13 20:23:29 +0200

    add sort constants

---
+++ constants/sort.ts
@@ -0,0 +1,6 @@
+export const SORT: { label: string; value: string }[] = [
+  { label: "Relevant", value: "relevant" },
+  { label: "Recent", value: "recent" },
+  { label: "Salary: Low to High", value: "salaryAsc" },
+  { label: "Salary: High to Low", value: "salaryDesc" },
+];
