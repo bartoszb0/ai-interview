9fa554a84367bd5017cad80d29c0a48da48c90a0
bartoszb0 <bartoszb2020@gmail.com>
2026-05-13 19:22:12 +0200

    refactor theme toggle

--- components/common/theme-toggle.tsx
+++ components/common/theme-toggle.tsx
@@ -9,13 +9,11 @@ export default function ThemeToggle() {
 
   return (
     <Button
-      variant="outline"
-      size="icon"
+      variant="ghost"
       onClick={() => setTheme(theme === "dark" ? "light" : "dark")}
     >
-      <Sun className="h-[1.2rem] w-[1.2rem] scale-100 rotate-0 transition-all dark:scale-0 dark:-rotate-90" />
-      <Moon className="absolute h-[1.2rem] w-[1.2rem] scale-0 rotate-90 transition-all dark:scale-100 dark:rotate-0" />
-      <span className="sr-only">Toggle theme</span>
+      {theme === "dark" ? <Sun className="transition-all" /> : <Moon />}
+      <span>{theme === "dark" ? "Light" : "Dark"} theme</span>
     </Button>
   );
 }
