ad98f02062a39d9a1a2dc5c5ad4d321ca4367c68
bartoszb0 <bartoszb2020@gmail.com>
2026-06-17 19:36:39 +0200

    feat: update ProfileBtn to conditionally render menu items based on authentication state

--- frontend/app/_components/ProfileBtn.tsx
+++ frontend/app/_components/ProfileBtn.tsx
@@ -10,10 +10,14 @@ import {
   DropdownMenuSeparator,
   DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu";
+import { useAuthStore } from "@/store/auth-store";
 import { User } from "lucide-react";
 import Link from "next/link";
 
 export default function ProfileBtn() {
+  const token = useAuthStore((state) => state.token);
+  const logout = useAuthStore((state) => state.logout);
+
   return (
     <DropdownMenu>
       <DropdownMenuTrigger asChild>
@@ -22,14 +26,25 @@ export default function ProfileBtn() {
         </Button>
       </DropdownMenuTrigger>
       <DropdownMenuContent align="end" className="rounded-none cursor-pointer">
-        <DropdownMenuGroup>
-          <DropdownMenuItem asChild>
-            <Link href={"/login"}>Login</Link>
-          </DropdownMenuItem>
-          <DropdownMenuItem asChild>
-            <Link href={"/register"}>Register</Link>
-          </DropdownMenuItem>
-        </DropdownMenuGroup>
+        {token ? (
+          <DropdownMenuGroup>
+            <DropdownMenuItem asChild>
+              <Link href={"/sessions"}>My sessions</Link>
+            </DropdownMenuItem>
+            <DropdownMenuItem className="text-red-400" onClick={logout}>
+              Logout
+            </DropdownMenuItem>
+          </DropdownMenuGroup>
+        ) : (
+          <DropdownMenuGroup>
+            <DropdownMenuItem asChild>
+              <Link href={"/login"}>Login</Link>
+            </DropdownMenuItem>
+            <DropdownMenuItem asChild>
+              <Link href={"/register"}>Register</Link>
+            </DropdownMenuItem>
+          </DropdownMenuGroup>
+        )}
         <DropdownMenuSeparator />
         <DropdownMenuGroup>
           <DropdownMenuItem asChild>
