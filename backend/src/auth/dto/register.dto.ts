e5450c2f84acf5470846d40ac2ead0d14ff9e995
bartoszb0 <bartoszb2020@gmail.com>
2026-06-17 19:01:43 +0200

    add max length to login and register dto

--- backend/src/auth/dto/register.dto.ts
+++ backend/src/auth/dto/register.dto.ts
@@ -1,13 +1,15 @@
-import { IsEmail, IsString, MinLength } from 'class-validator';
+import { IsEmail, IsString, MaxLength, MinLength } from 'class-validator';
 export class RegisterDto {
   @IsEmail()
   declare email: string;
 
   @IsString()
   @MinLength(6)
+  @MaxLength(24)
   declare password: string;
 
   @IsString()
   @MinLength(6)
+  @MaxLength(24)
   declare confirmPassword: string;
 }
