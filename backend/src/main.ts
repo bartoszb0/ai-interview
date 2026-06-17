bfd4c136dd8ce58ebcfcacf8783d938698ad3cdb
bartoszb0 <bartoszb2020@gmail.com>
2026-06-17 13:28:13 +0200

    fix: enable transformation in ValidationPipe for improved data handling

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -6,7 +6,7 @@ import { PrismaExceptionFilter } from './common/filters/prisma-exception.filter'
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
+  app.useGlobalPipes(new ValidationPipe({ transform: true, whitelist: true }));
   app.useGlobalFilters(new PrismaExceptionFilter());
   await app.listen(process.env.PORT ?? 3000);
 }
